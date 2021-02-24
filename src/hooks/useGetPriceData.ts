a2fdb60997b538b9ff2a013b87bbc87525c60231
tacofinance <tacoswapfinance@gmail.com>
2021-02-24 08:13:14 +0100

    chore: rename

--- src/hooks/useGetPriceData.ts
+++ src/hooks/useGetPriceData.ts
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react'
 import BigNumber from 'bignumber.js'
 import { useMulticallContract } from './useContract'
 import ERC20_INTERFACE from '../constants/abis/erc20'
-import priceContracts from '../constants/eggPriceContracts'
+import priceContracts from '../constants/tacoPriceContracts'
 
 type ApiResponse = {
   prices: {
@@ -20,23 +20,23 @@ const api = 'https://api.pancakeswap.com/api/v1/price'
 const useGetPriceData = () => {
   const [data, setData] = useState<number>(0)
 
-  const multicallContract = useMulticallContract();
+  const multicallContract = useMulticallContract()
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        if(multicallContract){
-          const {cakeAddress, busdAddress, lpAddress} = priceContracts;
+        if (multicallContract) {
+          const { cakeAddress, busdAddress, lpAddress } = priceContracts
           const calls = [
-            [cakeAddress, ERC20_INTERFACE.encodeFunctionData("balanceOf", [lpAddress])],
-            [busdAddress, ERC20_INTERFACE.encodeFunctionData("balanceOf", [lpAddress])],
-          ];
+            [cakeAddress, ERC20_INTERFACE.encodeFunctionData('balanceOf', [lpAddress])],
+            [busdAddress, ERC20_INTERFACE.encodeFunctionData('balanceOf', [lpAddress])],
+          ]
 
-          const [resultsBlockNumber, result] = await multicallContract.aggregate(calls);
-          const [cakeAmount, busdAmount] = result.map(r=>ERC20_INTERFACE.decodeFunctionResult("balanceOf", r));
-          const cake = new BigNumber(cakeAmount);
-          const busd = new BigNumber(busdAmount);
-          const cakePrice = busd.div(cake).toNumber();
+          const [resultsBlockNumber, result] = await multicallContract.aggregate(calls)
+          const [cakeAmount, busdAmount] = result.map((r) => ERC20_INTERFACE.decodeFunctionResult('balanceOf', r))
+          const cake = new BigNumber(cakeAmount)
+          const busd = new BigNumber(busdAmount)
+          const cakePrice = busd.div(cake).toNumber()
           setData(cakePrice)
         }
       } catch (error) {
